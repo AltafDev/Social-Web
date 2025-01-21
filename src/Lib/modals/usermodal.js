220c466f91aaddb8c49b833923ca13c39b72fb01
AltafDev <altafkhan1743@gmail.com>
2025-01-21 09:47:55 -0800

    hd

--- src/Lib/modals/usermodal.js
+++ src/Lib/modals/usermodal.js
@@ -18,10 +18,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    username: {
-      type: String,
-      required: true,
-    },
+   
     avatar: {
       type: String,
       required: true,
