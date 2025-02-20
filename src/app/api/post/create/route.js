84bb87611798764f621d73b0b51b6a0b92012b0a
AltafDev <altafkhan1743@gmail.com>
2025-02-20 07:41:31 -0800

    sfdgdsg

--- src/app/api/post/create/route.js
+++ src/app/api/post/create/route.js
@@ -1,35 +1,31 @@
-import Post from "../../../../Lib/modals/post.model"
-import { Connect } from "../../../../Lib/mongodb/mongodb"
-import { currentUser } from "@clerk/nextjs/server"
+import Post from "../../../../Lib/modals/post.model";
+import { Connect } from "../../../../Lib/mongodb/mongodb";
+import { currentUser } from "@clerk/nextjs/server";
 
-export async function POST(req) {
-    const user = await currentUser(req)
+export  async function POST(req) {
+    const user = await currentUser(req);
     try {
-        await Connect()
-        const data = await req.json()
+        await Connect();
+        const data = await req.json();
+
         if (!user || user.publicMetadata.userMongoId !== data.userMongoId) {
-            return new Response ("unauthorized", {
-                status:401
-            })
+            return new Response("Unauthorized", { status: 401 });
         }
+
         const newPost = await Post.create({
-            user:data.userMongoId,
-            name:data.name,
-            username:data.username,
-            text:data.text,
-            profileImg:data.profileImg,
-            image:data.image
-        })
+            user: data.userMongoId,
+            name: data.name,
+            username: data.username,
+            text: data.text,
+            profileImg: data.profileImg,
+            image: data.image,
+        });
 
-        await newPost.save()
-        return new Response (json.stringify(newPost),
-        {status:200}
-    )
-      
+        await newPost.save();
 
+        return new Response(JSON.stringify(newPost), { status: 200 });
     } catch (error) {
-            console.log(error)
-            return new Response ("Internal Server error"), 
-            {status:500}
+        console.error(error);
+        return new Response("Internal Server Error", { status: 500 });
     }
 }
