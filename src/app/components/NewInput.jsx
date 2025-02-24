61ddaaa44661e043b879a256ede24630d719c652
AltafDev <altafkhan1743@gmail.com>
2025-02-24 07:37:30 -0800

    final

--- src/app/components/NewInput.jsx
+++ src/app/components/NewInput.jsx
@@ -24,7 +24,7 @@ export default function Input() {
     setPostLoading(true);
     alert("hi")
     const response=await fetch(`/api/post/create`,{
-      method:"Post",
+      method:"POST",
       headers:{
         "Content-Type":"application/json"
       },
