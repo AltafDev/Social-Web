8f25d791aa6d6d7e661bb659da20d5f11f00ee47
AltafDev <altafkhan1743@gmail.com>
2025-02-25 09:20:47 -0800

    dsaf

--- src/app/api/post/create/route.js
+++ src/app/api/post/create/route.js
@@ -2,7 +2,7 @@ import POST from "../../../../Lib/modals/post.model";
 import { Connect } from "../../../../Lib/mongodb/mongodb";
 import { currentUser } from "@clerk/nextjs/server";
 
-export async function Post (req) {
+export async function NewPOST (req) {
   const user = await currentUser(req);
 
   try {
