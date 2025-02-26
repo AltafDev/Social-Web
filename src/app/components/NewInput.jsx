a20b87c1838e9beeeb7cfb0c3fd61e0dc7bb4337
AltafDev <altafkhan1743@gmail.com>
2025-02-26 08:33:00 -0800

    "done"

--- src/app/components/NewInput.jsx
+++ src/app/components/NewInput.jsx
@@ -41,6 +41,7 @@ export default function Input() {
     setSelectedImage(null)
   }
   return (
+   <>
     <div className="w-[60vh] flex flex-col p-4 border rounded-lg shadow-lg bg-white  space-y-4">
       <textarea
         value={input}
@@ -49,7 +50,24 @@ export default function Input() {
         className="w-full p-2 border rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-indigo-500"
         placeholder="Write your post..."
       />
-      {/* <div className="flex justify-between">
+  
+
+      <div className="flex  justify-between">
+        <CldUploadWidget uploadPreset="Social_web" onUpload={UploadImageHandler}>
+          {({ open }) => {
+            return <button onClick={() => open()}>Upload an Image</button>;
+          }}
+        </CldUploadWidget>
+
+        <button
+          onClick={PostUploadImage}
+          className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600"
+        >
+          Post
+        </button>
+      </div>
+    </div>
+        {/* <div className="flex justify-between">
        <div>
           <FaImage
             size={24}
@@ -80,21 +98,6 @@ export default function Input() {
           <img src={selectedImage} alt="Uploaded" className="w-full rounded-lg shadow-md" />
         </div>
       )} */}
-
-      <div className="flex  justify-between">
-        <CldUploadWidget uploadPreset="Social_web" onUpload={UploadImageHandler}>
-          {({ open }) => {
-            return <button onClick={() => open()}>Upload an Image</button>;
-          }}
-        </CldUploadWidget>
-
-        <button
-          onClick={PostUploadImage}
-          className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600"
-        >
-          Post
-        </button>
-      </div>
-    </div>
+   </>
   );
 }
