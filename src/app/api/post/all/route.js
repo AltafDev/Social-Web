0c6cc062a9baf84dbca9563be0e7bcad8f2a743e
AltafDev <altafkhan1743@gmail.com>
2025-03-18 05:59:09 -0700

    hello

--- src/app/api/post/all/route.js
+++ src/app/api/post/all/route.js
@@ -1,15 +1,13 @@
 import Post from "../../../../Lib/modals/post.model";
 import { Connect } from "../../../../Lib/mongodb/mongodb";
 
-export const POST=async(req)=>{
-    try {
-        await Connect()
-        const feedposts=await Post.find().sort({createdAt:-1})
-        return new Response(JSON.stringify(feedposts),{
-            status:200
-        })
 
+export const POST = async (req) => {
+    try {
+        await Connect();
+        const feedPosts = await Post.find().sort({ createdAt: -1 });
+        return new Response(JSON.stringify(feedPosts), { status: 200 });
     } catch (error) {
-        console.log()
+        console.error("Error fetching posts:", error);
     }
-}
+    }
