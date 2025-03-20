b8db45e7a693a3dd7cc62753b00de64ff47dbef4
AltafDev <altafkhan1743@gmail.com>
2025-03-20 05:50:16 -0700

    "DONE

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,24 @@
 
 import React from 'react'
 import NewInput from "./components/NewInput"
-// import "./components/PostStyle.css"
-// import { AiFillLike } from "react-icons/ai";
-// import { FaComments } from "react-icons/fa6";
-// import { FaShare } from "react-icons/fa";
+import "./components/PostStyle.css"
+import { AiFillLike } from "react-icons/ai";
+import { FaComments } from "react-icons/fa6";
+import { FaShare } from "react-icons/fa";
 
 export default async function page () {
-// const HomeImg = `https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=`
-//   let data=null
-  // try {
-  //   const result=await fetch("https://social-web-qdd9-git-main-altafdevs-projects-be8e2cbb.vercel.app/api/post/all",{
-  //     method:"POST",
-  //     cache:"no-store"
-  //     headers:{
-  //       "Content-Type":"application/json"
-  
-  //     },
-  //   })
-  //   data=await result.json()
-  // } catch (error) {
-  //   console.log(error)
-  // }
-  // console.log(data)
+const HomeImg = `https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=`
+  let data=null
+  try {
+    const result=await fetch("https://social-web-qdd9-git-main-altafdevs-projects-be8e2cbb.vercel.app/api/post/all",{
+      method:"POST",
+      cache:"no-store"
+    })
+    data=await result.json()
+  } catch (error) {
+    console.log(error)
+  }
+  console.log(data)
   return (
     <>
      <NewInput/>
@@ -30,7 +26,7 @@ export default async function page () {
 <div>
 
  
-  {/* {
+  {
     data.map((item)=>(
       
       <>
@@ -70,7 +66,7 @@ export default async function page () {
  </div>
       </>
     ))
-  } */}
+  }
 </div>
     </>
   )
