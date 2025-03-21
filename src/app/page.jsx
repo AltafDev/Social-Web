bc7a8dbb044448190ab538c609acc3e02b984169
AltafDev <altafkhan1743@gmail.com>
2025-03-21 06:13:58 -0700

    "data set"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,33 @@
 
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewInput from "./components/NewInput"
 import "./components/PostStyle.css"
 import { AiFillLike } from "react-icons/ai";
 import { FaComments } from "react-icons/fa6";
 import { FaShare } from "react-icons/fa";
+import axios from "axios";
 
-export default async function page () {
+export default  function page () {
 const HomeImg = `https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=`
  useEffect(() => {
   GetData()
  }, [])
  
 
-let data=null
-  const GetData = async() => {
+const [data,setData]=useState([])
+  const GetData = () => {
     try {
-      const result=await fetch("https://social-web-qdd9.vercel.app/api/post/all",{
-        method:"POST",
-        cache:"no-store"
-      })
-      data=await result.json()
+      axios.post("https://social-web-qdd9.vercel.app/api/post/all")
+      .then(res =>setData(res.data) )
+      .catch(err => console.error(err));
+      
     } catch (error) {
       console.log(error)
     }
+
+
+
   }
 
   console.log(data)
