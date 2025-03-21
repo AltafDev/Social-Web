9261455b5d9c67f0a0b9a53f80d07a69e1b640cd
AltafDev <altafkhan1743@gmail.com>
2025-03-21 06:04:16 -0700

    "data set"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,19 +8,25 @@ import { FaShare } from "react-icons/fa";
 
 export default async function page () {
 const HomeImg = `https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=`
+ useEffect(() => {
+  GetData()
+ }, [])
  
 
 let data=null
-  // try {
-  //   const result=await fetch("/api/post/all",{
-  //     method:"POST",
-  //     cache:"no-store"
-  //   })
-  //   data=await result.json()
-  // } catch (error) {
-  //   console.log(error)
-  // }
-  // console.log(data)
+  const GetData = async() => {
+    try {
+      const result=await fetch("https://social-web-qdd9.vercel.app/api/post/all",{
+        method:"POST",
+        cache:"no-store"
+      })
+      data=await result.json()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(data)
   return (
     <>
      <NewInput/>
